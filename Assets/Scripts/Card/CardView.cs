3b9c2118bb5fdce9fd6599d9568ea24660babfbb
Bex <rebeccaruva@newschool.edu>
2022-10-12 09:34:44 +0200

    Added sounds, fixed animation

--- Assets/Scripts/Card/CardView.cs
+++ Assets/Scripts/Card/CardView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Threading.Tasks;
 
 public class CardView
 {
@@ -46,15 +47,37 @@ public class CardView
             Deselect();
         }
     }
-    private void Select()
+    private async void Select()
     {
         if (isSelected == true) return;
 
-        Vector2 newPosition = controller.transform.position;
-        newPosition.y += 1;
-        controller.transform.position = newPosition;
-        isSelected = true;
+        Vector2 startPosition = controller.transform.position;
+        Vector2 targetPosition = new Vector3(startPosition.x, startPosition.y + 1);
+        //Vector2 currentPosition = startPosition;
+        //disable collider during animation
+        Collider2D[] colliders = controller.GetComponents<Collider2D>();
+        foreach (Collider2D col in colliders)
+        {
+            col.enabled = false;
+        }
 
+        float velocity = 0.01f;
+
+        //lerp between start and final position
+        while (new Vector2(controller.transform.position.x, controller.transform.position.y) != targetPosition)
+        {
+            controller.transform.position = Vector2.Lerp(controller.transform.position, targetPosition, velocity);
+            velocity += 0.0001f;
+            await Task.Delay(1);
+        }
+
+        //enable collider again
+        foreach (Collider2D col in colliders)
+        {
+            col.enabled = true;
+        }
+
+        isSelected = true;
         controller.OnSelected(true);
     }
     public void Deselect()
