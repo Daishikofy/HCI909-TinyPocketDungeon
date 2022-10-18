c2473de6669ecd18687967214aecd1160a24de0e
Daishikofy <daishi.setsuko@gmail.com>
2022-10-18 17:20:25 +0200

    Added score on main menu

--- Assets/Scripts/MainMenu/MainMenuUiManager.cs
+++ Assets/Scripts/MainMenu/MainMenuUiManager.cs
@@ -1,16 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainMenuUiManager : MonoBehaviour
 {
     [SerializeField]
     LevelsPanel _levelsPanel;
 
+    [SerializeField]
+    TextMeshProUGUI _scoreText;
+
+
     // Start is called before the first frame update
     void Awake()
     {
         _levelsPanel.gameObject.SetActive(true);
+    }
+
+    private void Start()
+    {
+        _scoreText.text = PlayerPrefs.GetInt("Score").ToString();
     }
 
     public void ShowLevelMenu(bool value)
